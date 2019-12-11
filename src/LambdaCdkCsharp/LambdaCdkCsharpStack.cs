0cceec1e9ef895edfe88d555e91e2f22e4f044a9
Niranjan Jayakar <nija@amazon.com>
2019-12-11 13:51:12 +0000

    CDK app initialization
    Generated by 'cdk init -l=csharp'

---
+++ src/LambdaCdkCsharp/LambdaCdkCsharpStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace LambdaCdkCsharp
+{
+    public class LambdaCdkCsharpStack : Stack
+    {
+        internal LambdaCdkCsharpStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ src/LambdaCdkCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaCdkCsharp
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new LambdaCdkCsharpStack(app, "LambdaCdkCsharpStack");
+            app.Synth();
+        }
+    }
+}
